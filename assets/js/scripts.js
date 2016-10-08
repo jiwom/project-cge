c6bedeb4b6d0b92efb163e5c6bc8b1c6cf2d8354
Jerome Sarmiento <jsarmiento@ltcglobal.com>
2016-10-08 15:17:44 +0800

    Update with betting function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,25 @@
+//https://github.com/jiwom/project-cge.git
 $(document).ready(function(){
-	var kulay = ["green", "pink", "orange", "red", "yellow", "blue"];
+	var kulay = ["green", "pink", "white", "red", "yellow", "blue"];
+	var pera = $('#pera').text();
     var taya = 5;
+    var winningColors = [];
 
     changeColor();
+    
     $('#dice-pull').click(function(){
     	rollDice();
     });
+
+    $('.bet-box').click(function(){
+		if(pera > 0){
+			var total = parseInt($(this).text());
+			pera = pera - taya;
+			total += taya;
+			$(this).text(total);
+			$('#pera').text(pera);
+		}
+	});
     
     function changeColor(){
 		for(var i = 0; i< 3; i++){
@@ -16,9 +30,36 @@ $(document).ready(function(){
 	};
 
 	function rollDice(){
+		var count = 0;
 		for(var i = 0; i<= 10000; i++){
-			setTimeout(function(){ changeColor() }, 100);
+			setTimeout(
+				function(){
+					changeColor();
+					if(count == 10000){
+						computePera();
+					}
+					count++;
+				}
+			,100);
 		}
+	}
+
+	function computePera() {
+		//get winning colors
+		winningColors = [$('#dice-0').data('color'),$('#dice-1').data('color'),$('#dice-2').data('color')];
+		console.log(winningColors);
+
+		//get taya of each winning colors
+		var winBoxAmount1 = parseInt($('#box-'+winningColors[0]).text()) * 2;
+		var winBoxAmount2 = parseInt($('#box-'+winningColors[1]).text()) * 2;
+		var winBoxAmount3 = parseInt($('#box-'+winningColors[2]).text()) * 2;
+		console.log(winBoxAmount1, winBoxAmount2, winBoxAmount3);
+
+		pera = pera + winBoxAmount1 + winBoxAmount2 + winBoxAmount3;
+		$('#pera').text(pera);
+
+		//reset others to zero
+		$('.bet-box').text(0);
 	}
 
 });
